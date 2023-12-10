c8e2826fc54a0faa0c489532909e52638dc6fac9
Michael Raheem <abiolar544@gmail.com>
2023-12-10 06:50:41 +0100

    reset

--- console.py
+++ console.py
@@ -3,6 +3,7 @@
 import cmd
 
 class HBNBCommand(cmd.Cmd):
+    """Command interpreter for the HBNB console"""
     prompt = '(hbnb) '
 
     def do_quit(self, arg):
@@ -15,6 +16,31 @@ class HBNBCommand(cmd.Cmd):
 
     def emptyline(self):
         """Do nothing when an empty line is entered"""
+        pass
+    
+    def do_create(self, arg):
+        """Create command implementation."""
+        # Implement logic to create an instance based on arg
+        pass
+
+    def do_show(self, arg):
+        """Show command implementation."""
+        # Implement logic to show details of an instance
+        pass
+
+    def do_destroy(self, arg):
+        """Destroy command implementation."""
+        # Implement logic to delete an instance based on arg
+        pass
+
+    def do_all(self, arg):
+        """All command implementation."""
+        # Implement logic to show all instances
+        pass
+
+    def do_update(self, arg):
+        """Update command implementation."""
+        # Implement logic to update an instance
         pass
 
 if __name__ == '__main__':
